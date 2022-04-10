06a89a0a48aeb5d1041426ef4fd264b3b7783e6c
julija2202 <julietkajagolovich@gmail.com>
2022-04-10 19:40:09 +0300

    doing_2

--- src/Ship.java
+++ src/Ship.java
@@ -12,11 +12,12 @@ public class Ship {
         Deck deck = new Cylinder();
         Deck deck_1 = new Cone();
         Deck deck_2 = new Square();
+        Double[] allmassa = new Double[10];
         for (int i = 0; i < 10; i++) {
             int elements = (int) (Math.random() * (7 - 1) + 1);
             ships[i] = elements;
-            System.out.println(ships[i]);
-            Double[] allmassa = new Double[10];
+           // System.out.println(ships[i]);
+
             int elements_2 = (int) (Math.random() * (11 - 1) + 0);
             allmassa[i] = (double) elements_2;
             if (elements == 1) {
@@ -32,12 +33,17 @@ public class Ship {
             } else if (elements == 6) {
                 allmassa[i] = ((double) deck_2.volume()) * 2;
             }
-            System.out.println(allmassa[i]);
-            //    double sum = 0;
-           //     for (double num: allmassa) {
-            //        sum = sum + num;
-            //    }
-                //System.out.println(sum);
+            System.out.println(allmassa[i]);}
+            double sum = 0;
+            for (double num: ships) {
+                sum = sum + num;
             }
-        }
+            //System.out.println(sum);
+            double sum_2 = 0;
+            for (double num: allmassa) {
+            sum_2 = sum_2 + num;
+    }
+            System.out.println("Объем воды, которую привели корабли = " + sum_2);
+            System.out.println("Масса воды, которую привезли корабли = " + (sum_2 * 1000));
 }
+        }
